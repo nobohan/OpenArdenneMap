54f8894ec78e316dd3822759d922846169992c84
nobohan <juminet@gmail.com>
2018-11-09 20:50:18 +0100

    change to osm2pgsql - work in progress

--- create-db.sh
+++ create-db.sh
@@ -1,14 +1,10 @@
 # run this as postgres user, eg:
-# imposm-psqldb > create_db.sh; sudo su postgres; sh ./create_db.sh
-set -xe
-createuser --no-superuser --no-createrole --createdb osm
-createdb -E UTF8 -O osm osm
-psql -d osm -c "alter user osm with password 'osm';"
-psql -d osm -c "create extension postgis;"
-psql -d osm -f YOUR PATH TO VENV: /venv/local/lib/python2.7/site-packages/imposm/900913.sql
-echo "ALTER TABLE geometry_columns OWNER TO osm;" | psql -d osm
-echo "ALTER TABLE spatial_ref_sys OWNER TO osm;" | psql -d osm
-echo "ALTER USER osm WITH PASSWORD 'osm';" |psql -d osm
-echo "host	osm	osm	127.0.0.1/32	md5" >> /etc/postgresql/9.5/main/pg_hba.conf 	# <- CHANGE THIS PATH
-set +x
-echo "Done. Don't forget to restart postgresql!"
+# sudo -u postgres -i; sh ./create_db.sh
+createuser osmpg
+createdb -E UTF8 -O osmpg osmpg_db
+psql -d osmpg_db -c "CREATE EXTENSION postgis;"
+psql -d osmpg_db -c "CREATE EXTENSION hstore;"
+psql -d osmpg_db -c "ALTER TABLE geometry_columns OWNER TO osmpg;"
+# Then, add the user
+#sudo useradd -m  osmpg
+#sudo passwd osmpg
