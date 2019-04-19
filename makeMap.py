d306cad69fc9605dbe35f21c943d4a495dfc82d6
nobohan <juminet@gmail.com>
2019-04-19 08:10:30 +0200

    merge master into branch (rebase master)

--- makeMap.py
+++ makeMap.py
@@ -1,40 +1,30 @@
 # OpenArdenneMap - Pilot mapnik with python - June 2017
-
 import math
-from mapnik import Map, Envelope, render_to_file, load_map
+from mapnik import Envelope, Map, load_map, render_to_file
 
-PAGE_FORMAT = 'A4'
-PAGES = (4, 3, 2, 1, 0)
+def make_map(distance, scale, x_center = 622000, y_center = 6406400, pageFormat = 'A3'):
+    # The mapFile is generated using carto from cartoCSS files.
+    # /!\ Choose the right importer here: osm2pgsql or imposm!
+    mapFile = 'osm2pgsql/OpenArdenneMap' + distance + '.xml'
 
-X_CENTER = 616167
-Y_CENTER = 6398834
-LATITUDE = 50 # in degrees
+    # The mapOutput can be have different extension; png, pdf, svg, ...
+    mapOutput = 'marbehan_' + distance + '.pdf'
 
-# The mapFile is generated using carto from cartoCSS files.
-# /!\ Choose the right importer here: osm2pgsql or imposm!
-MAPNIK_FILE = 'osm2pgsql/OpenArdenneMap.xml'
-
-
-def make_map(map_output, scale=20000):
-    "Make a map as a function of the scale"
-
+    # Map size
+    page = pageFormat
+    pages = (4, 3, 2, 1, 0)
     # Compute the scale
-    page_size = int(PAGE_FORMAT[1])
-    f = math.sqrt(2)**PAGES[page_size]
+    page_size = int(page[1])
+    f1 = pages[page_size]
+    f = math.sqrt(2)**f1
 
-    # increasing map_x and map_y lead to decreasing font and symbol size: not good
     map_x = int(f*4600)  # 4600 is the number of pixels for an A4 length
-
-    # if 460: zoom = 13
     map_y = int(map_x/math.sqrt(2))
     m = Map(map_x, map_y)
-    load_map(m, MAPNIK_FILE)
+    load_map(m, mapFile)
 
     # Bounding box (expressed in EPSG:3857, meters)
-    x_center = X_CENTER
-    y_center = Y_CENTER
-
-    delta_x = f*0.295*scale/math.cos(LATITUDE*2*math.pi/360)
+    delta_x = f*0.295*scale/math.cos(50*2*math.pi/360)
     delta_y = delta_x/math.sqrt(2)
     xmin = x_center - delta_x/2
     xmax = x_center + delta_x/2
@@ -45,10 +35,12 @@ def make_map(map_output, scale=20000):
     m.zoom_to_box(bbox)
     print("Scale = " + str(m.scale()))
 
-    render_to_file(m, map_output)
+    # Export to mapOutput
+    render_to_file(m, mapOutput)
 
 
-
-make_map('OAM_5000.pdf', 5000)
-make_map('OAM_20000.pdf', 20000)
-make_map('OAM_100000.pdf', 100000)
+make_map('Marche', 20000, 618000, 6398000, 'A4')
+make_map('28km', 25000, 621000, 6403500)
+make_map('32km', 25000, 621000, 6403500)
+make_map('42km', 30000, 621000, 6406000)
+make_map('56km', 38000)
---
+++ run.sh
@@ -0,0 +1,6 @@
+carto osm2pgsql/projectMarche.mml > osm2pgsql/OpenArdenneMapMarche.xml &&
+carto osm2pgsql/project28km.mml > osm2pgsql/OpenArdenneMap28km.xml &&
+carto osm2pgsql/project32km.mml > osm2pgsql/OpenArdenneMap32km.xml &&
+carto osm2pgsql/project42km.mml > osm2pgsql/OpenArdenneMap42km.xml &&
+carto osm2pgsql/project56km.mml > osm2pgsql/OpenArdenneMap56km.xml &&
+python3 makeMap.py
